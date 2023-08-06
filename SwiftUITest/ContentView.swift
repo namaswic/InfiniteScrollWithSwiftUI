d8d437993438718432bf388e8ae3bcbbfb3aa3e4
Namaswi <namaswic@gmail.com>
2023-08-05 21:35:38 -0400

    first commit

--- SwiftUITest/ContentView.swift
+++ SwiftUITest/ContentView.swift
@@ -7,20 +7,80 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AsyncPicture: View {
+    private let url: String
+
+    init(url: String) {
+        self.url = url
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        AsyncImage(url: URL(string: url)) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+            case .success(let image):
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            case .failure:
+                Image(systemName: "exclamationmark.icloud.fill")
+            @unknown default:
+                EmptyView()
+            }
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    @StateObject var viewModel = ImageViewModel()
+
+    var body: some View {
+        List(viewModel.images) { image in
+            AsyncImage(url: URL(string: image.download_url)) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+                case .success(let image):
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                case .failure:
+                    Image(systemName: "exclamationmark.icloud.fill")
+                @unknown default:
+                    EmptyView()
+                }
+            }
+        }
+        .navigationTitle("SwiftUI Infinite Scroll")
+        .onAppear(perform: viewModel.fetchData)
     }
+}
+
+class ImageViewModel: ObservableObject {
+    @Published var images = [ImageModel]()
+    private var page = 1
+    private let limit = 100
+
+    func fetchData() {
+        guard let url = URL(string: "https://picsum.photos/v2/list?page=1&limit=100") else { return }
+        let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            guard let data = data, error == nil else { return }
+            let decoder = JSONDecoder()
+            do {
+                let images = try decoder.decode([ImageModel].self, from: data)
+                DispatchQueue.main.async {
+                    self.images.append(contentsOf: images)
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+}
+
+struct ImageModel: Codable, Identifiable {
+    let id: String
+    let download_url: String
 }
